e4dba3031341576516fff75b9c2955afbb4d3b22
avi <gajai.avinash@gmail.com>
2024-07-12 17:47:59 -0400

    Exercises one through 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (nums.includes(arr[i])) {
+            arr.splice(i, 1);
+        }
+    }
+    return arr;
 };
 
-// Do not edit below this line
+
 module.exports = removeFromArray;
