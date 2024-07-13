8bb4a979bfcf7bc6814fb54a9e7ce0fa80b0a96e
avi <gajai.avinash@gmail.com>
2024-07-13 01:46:40 -0400

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,43 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    const Array = [];
+    let sum = 0;
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+
+    else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            Array.push(i);
+        }
+
+        for (let i = 0; i < Array.length; i++) {
+            sum += Array[i];
+        }
+    }
+
+    else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            Array.push(i);
+        }
+
+        for (let i = 0; i < Array.length; i++) {
+            sum += Array[i];
+        }
+
+    }
+
+    else {
+        return "ERROR";
+    }
+
+
+    return sum;
+
 
 };
 
